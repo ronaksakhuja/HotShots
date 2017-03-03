859206e87fca2b2a82b9988fc286e3dd5bcb1dcf
ronaksakhuja@gmail.com <ronaksakhuja@gmail.com>
2017-03-03 22:34:27 +0530

    Added database support and Dialogbox for input

--- app/src/main/java/com/sodevan/hotshots/MainActivity.java
+++ app/src/main/java/com/sodevan/hotshots/MainActivity.java
@@ -1,14 +1,29 @@
 package com.sodevan.hotshots;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.sodevan.hotshots.Activities.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt= (Button) findViewById(R.id.loginbutton);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
+
 
 
     }
